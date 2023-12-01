45f863be4360ddedd953a5b20b04fc7bb32e9426
graceblessing <blessedgrace097@gmail.com>
2023-12-01 14:45:58 +0100

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>devlodrty</div>
+  )
+}
+import React from 'react'
+
+ const App = () => {
+  return (
+    <div>
+      <h1>dyytrdutyuiuy</h1>
+    </div>
+  )
+}
